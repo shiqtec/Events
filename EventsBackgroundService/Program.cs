0e0826a6225370684cf395c030ae1d5a897fb583
Ashiq <shiqtec@gmail.com>
2022-03-30 23:10:43 +0200

    Add TickerService that invokes a time event and add subscribers

--- EventsBackgroundService/Program.cs
+++ EventsBackgroundService/Program.cs
@@ -2,6 +2,7 @@ using EventsBackgroundService;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<TickerService>();
 builder.Services.AddHostedService<TickerBackgroundService>();
 
 var app = builder.Build();
--- EventsBackgroundService/TickerBackgroundService.cs
+++ EventsBackgroundService/TickerBackgroundService.cs
@@ -2,11 +2,18 @@
 {
     public class TickerBackgroundService : BackgroundService
     {
+        private readonly TickerService _tickerService;
+
+        public TickerBackgroundService(TickerService tickerService)
+        {
+            _tickerService = tickerService;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                Console.WriteLine($"Message");
+                _tickerService.OnTick(TimeOnly.FromDateTime(DateTime.Now));
                 await Task.Delay(1000, stoppingToken);
             }
         }
