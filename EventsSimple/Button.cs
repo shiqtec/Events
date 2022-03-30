adb70309d41a963febd5c8281c162ebd166f914a
Ashiq <shiqtec@gmail.com>
2022-03-30 19:51:33 +0200

    Add Button and ButtonPressedEventArgs

---
+++ EventsSimple/Button.cs
@@ -0,0 +1,12 @@
+﻿namespace EventsSimple
+{
+    public class Button
+    {
+        public event EventHandler ButtonPressed;
+
+        public void OnButtonPressed(char key)
+        {
+            ButtonPressed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ EventsSimple/ButtonPressedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace EventsSimple
+{
+    public class ButtonPressedEventArgs
+    {
+        public ButtonPressedEventArgs(char key)
+        {
+            Key = key;
+        }
+
+        public char Key { get; }
+    }
+}
