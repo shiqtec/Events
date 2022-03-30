3c7731cf71ee631e8c656785044db5b163d96322
Ashiq <shiqtec@gmail.com>
2022-03-30 20:02:06 +0200

    Add EventHandler type ButtonPressedEventArgs and update class name to ButtonMaster

---
+++ EventsSimple/ButtonMain.cs
@@ -0,0 +1,12 @@
+﻿namespace EventsSimple
+{
+    public class ButtonMain
+    {
+        public event EventHandler<ButtonPressedEventArgs>? ButtonPressed;
+
+        public void OnButtonPressed(char key)
+        {
+            ButtonPressed?.Invoke(this, new ButtonPressedEventArgs(key));
+        }
+    }
+}
