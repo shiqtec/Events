ab7173408693fa3eb420dcb15d8451ef3b1f4bb3
Ashiq <shiqtec@gmail.com>
2022-04-03 11:07:10 +0200

    Add inject the transient service and print out the GUID

--- EventsMediatR/EverySecondHandler.cs
+++ EventsMediatR/EverySecondHandler.cs
@@ -4,9 +4,16 @@ namespace EventsMediatR
 {
     public class EverySecondHandler : INotificationHandler<TimedNotification>
     {
+        private readonly TransientGUIDService _guidService;
+
+        public EverySecondHandler(TransientGUIDService guidService)
+        {
+            _guidService = guidService;
+        }
+
         public Task Handle(TimedNotification notification, CancellationToken cancellationToken)
         {
-            Console.WriteLine(notification.Time.ToLongTimeString());
+            Console.WriteLine(_guidService.guid);
             return Task.CompletedTask;
         }
     }
